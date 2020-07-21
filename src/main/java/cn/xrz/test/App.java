87df21aa6b87c15137b996fc6d7ed9fe0efd08f0
maweiye <1311496533@qq.com>
2020-07-21 19:46:42 +0800

    ceshi3

--- src/main/java/cn/xrz/test/App.java
+++ src/main/java/cn/xrz/test/App.java
@@ -2,6 +2,8 @@ package cn.xrz.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
-public class App {
+public class App extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
@@ -21,8 +30,8 @@ public class App {
 
     @GetMapping("/")
     public String t(){
-          return "66678";
-//        return "Hello JenKins!";
+//          return "66678";
+        return "Hello JenKins!";
 
     }
 }
